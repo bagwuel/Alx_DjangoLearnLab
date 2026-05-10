29898c9e7d178423ecb29bd7206a760d5aa4e179
bagwuel <mbagwu39@gmail.com>
2026-05-10 08:42:07 +0000

    advanced model relationship query_sample updated

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,11 @@
 author = Author.objects.get(name='Emmanuel')
 books = author.book_set.all()
 
-library = Library.objects.get(name='library_name')
-books = library.book.all()
+
+library_name = "mylibrary"
+library = Library.objects.get(name=library_name)
+books = library.book
+all_books = books.all()
 
 library = Library.objects.get(name='mylibrary')
 librarian = library.librarian_set
