23a947bcf39be372fa30fe4f441b5ce225ec6325
bagwuel <mbagwu39@gmail.com>
2026-05-16 05:47:16 +0000

    updated user registration, login, logout

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,8 +5,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
-        path('login/', LoginView.as_view(template_name = "relationship_app/login.html"), name='login'),
-        path('logout/', LogoutView.as_view(template_name = "relationship_app/logout.html"), name='logout'),
+        path('login/', LoginView.as_view(template_name="relationship_app/login.html"), name='login'),
+        path('logout/', LogoutView.as_view(template_name="relationship_app/logout.html"), name='logout'),
         path('register/', RegisterView.as_view(), name='register'),
         path('profile/', profile, name='profile'),
         path(
